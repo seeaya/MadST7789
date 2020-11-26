cfecfc38841f98f2eefa571cc8f1d78204310898
Andy Liu <andy@madmachine.io>
2020-11-26 19:26:36 +0800

    Update Nyan Cat example
    Signed-off-by: Andy Liu <andy@madmachine.io>

--- Examples/NyanCat/Sources/NyanCat/main.swift
+++ Examples/NyanCat/Sources/NyanCat/main.swift
@@ -7,7 +7,7 @@ let spi = SPI(Id.SPI0, speed: 60_000_000)
 let dc = DigitalOut(Id.D1)
 let rst = DigitalOut(Id.D0)
 let bl = DigitalOut(Id.D4)
-let screen = MadST7789(spi: spi, dc: dc, rst: rst, bl: bl, width: 240)
+let screen = MadST7789(spi: spi, dc: dc, rst: rst, bl: bl, width: 320)
 let display = MadDisplay(screen: screen)
 
 let catFile = BMP(path: "/SD:/image/nyanCat.bmp")
@@ -19,7 +19,7 @@ let palette = catFile.getPalette()
 
 let root = Group()
 
-let bgBitmap = Bitmap(width: 240, height: 240, bitCount: 1)
+let bgBitmap = Bitmap(width: 320, height: 240, bitCount: 1)
 let bgPalette = Palette(count: 1)
 bgPalette[0] = palette[15]
 
@@ -30,132 +30,86 @@ display.show(root)
 
 let cat1 = TileGrid(bitmap: catBitmap,
                     palette: palette,
-                    width: 2,
-                    height: 2,
-                    tileWidth: 32,
-                    tileHeight: 32)
+                    tileWidth: 128,
+                    tileHeight: 128)
 
 cat1.setTile(x: 0, y: 0, index: 0)
-cat1.setTile(x: 1, y: 0, index: 1)
-cat1.setTile(x: 0, y: 1, index: 6)
-cat1.setTile(x: 1, y: 1, index: 7)
 
 
 let cat2 = TileGrid(bitmap: catBitmap,
                     palette: palette,
-                    width: 2,
-                    height: 2,
-                    tileWidth: 32,
-                    tileHeight: 32)
+                    tileWidth: 128,
+                    tileHeight: 128)
 
-cat2.setTile(x: 0, y: 0, index: 2)
-cat2.setTile(x: 1, y: 0, index: 3)
-cat2.setTile(x: 0, y: 1, index: 8)
-cat2.setTile(x: 1, y: 1, index: 9)
+cat2.setTile(x: 0, y: 0, index: 1)
 
 
 let cat3 = TileGrid(bitmap: catBitmap,
                     palette: palette,
-                    width: 2,
-                    height: 2,
-                    tileWidth: 32,
-                    tileHeight: 32)
+                    tileWidth: 128,
+                    tileHeight: 128)
 
-cat3.setTile(x: 0, y: 0, index: 12)
-cat3.setTile(x: 1, y: 0, index: 13)
-cat3.setTile(x: 0, y: 1, index: 18)
-cat3.setTile(x: 1, y: 1, index: 19)
+cat3.setTile(x: 0, y: 0, index: 2)
 
 
 let cat4 = TileGrid(bitmap: catBitmap,
                     palette: palette,
-                    width: 2,
-                    height: 2,
-                    tileWidth: 32,
-                    tileHeight: 32)
+                    tileWidth: 128,
+                    tileHeight: 128)
 
-cat4.setTile(x: 0, y: 0, index: 14)
-cat4.setTile(x: 1, y: 0, index: 15)
-cat4.setTile(x: 0, y: 1, index: 20)
-cat4.setTile(x: 1, y: 1, index: 21)
+cat4.setTile(x: 0, y: 0, index: 3)
 
 let rainbow1 = TileGrid(bitmap: catBitmap,
                     palette: palette,
-                    width: 1,
-                    height: 2,
-                    tileWidth: 32,
-                    tileHeight: 32)
+                    tileWidth: 128,
+                    tileHeight: 128)
 rainbow1.setTile(x: 0, y: 0, index: 4)
-rainbow1.setTile(x: 0, y: 1, index: 10)
-
 
 
 let rainbow2 = TileGrid(bitmap: catBitmap,
                     palette: palette,
-                    width: 1,
-                    height: 2,
-                    tileWidth: 32,
-                    tileHeight: 32)
-rainbow2.setTile(x: 0, y: 0, index: 4)
-rainbow2.setTile(x: 0, y: 1, index: 10)
+                    tileWidth: 128,
+                    tileHeight: 128)
+rainbow2.setTile(x: 0, y: 0, index: 5)
 
-
-
-let rainbow3 = TileGrid(bitmap: catBitmap,
-                    palette: palette,
-                    width: 1,
-                    height: 2,
-                    tileWidth: 32,
-                    tileHeight: 32)
-rainbow3.setTile(x: 0, y: 0, index: 4)
-rainbow3.setTile(x: 0, y: 1, index: 10)
-
-let rainbow4 = TileGrid(bitmap: catBitmap,
-                    palette: palette,
-                    width: 1,
-                    height: 2,
-                    tileWidth: 32,
-                    tileHeight: 32)
-rainbow4.setTile(x: 0, y: 0, index: 4)
-rainbow4.setTile(x: 0, y: 1, index: 10)
-
+root.append(rainbow1)
 
 let name = TileGrid(bitmap: catBitmap,
                     palette: palette,
                     width: 2,
                     height: 1,
-                    tileWidth: 32,
-                    tileHeight: 32)
-name.setTile(x: 0, y: 0, index: 22)
-name.setTile(x: 1, y: 0, index: 23)
+                    tileWidth: 128,
+                    tileHeight: 128)
+name.setTile(x: 0, y: 0, index: 6)
+name.setTile(x: 1, y: 0, index: 7)
 
 
 
 root.append(name)
-root.append(rainbow1)
-root.append(rainbow2)
-root.append(rainbow3)
-root.append(rainbow4)
+name.setXY(x: 100, y: 100)
 
 
-
-name.setXY(x: 175, y: 200)
-rainbow1.setXY(x: 0, y: 96)
-rainbow2.setXY(x: 32, y: 96)
-rainbow3.setXY(x: 64, y: 96)
-rainbow4.setXY(x: 96, y: 96)
-
+let rainbows = [rainbow1, rainbow2]
 let cats = [cat1, cat2, cat3, cat4]
 
 
 
 while true {
+    var changeRainbow1 = true
     for cat in cats {
         root.append(cat)
-        cat.setXY(x: 128, y: 96)
+        cat.setXY(x: 128, y: 0)
+        if changeRainbow1 {
+            root.remove(rainbow1)
+            root.append(rainbow2)
+            changeRainbow1.toggle()
+        } else {
+            root.remove(rainbow2)
+            root.append(rainbow1)
+            changeRainbow1.toggle()
+        }
         display.show(root)
         root.remove(cat)
-        sleep(ms: 100)
     }
 }
 
